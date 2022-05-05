9100e6e7ebdf262fb624c3c6d6da9e2ad8cce3b6
Draraedus <adriel2002santos@gmail.com>
2022-05-05 20:53:06 -0300

    organização e testes com Tkinter

--- src/Application.py
+++ src/Application.py
@@ -1,21 +1,25 @@
 from tkinter import *
+from PIL import Image, ImageTk
 
 root = Tk()
 
 class Application:
     def __init__(self):
-        self.root = root    
+        self.root = root
         self.tela()
         self.rooms()
         self.addRoom()
         self.addRoomButton()
         root.mainloop()
+
     def tela(self):
         self.root.title("Projeto Cinema")
-        self.root.configure(background="black")
         self.root.geometry("788x988")
         self.root.resizable(True, True)
         self.root.minsize(width=488, height=788)
+        bg = ImageTk.PhotoImage(Image.open("C:/Users/Pichau/Documents/Estudos/Faculdade/POO/Projeto Final/src/background.jpg").resize((488, 788)))
+        label1 = Label(root, image = bg) 
+        label1.place(x=0, y=0, relwidth=1, relheight=1)
     # def tools(self):
     #     self.frameOfTools = Frame(self.root)
     #     self.frameOfTools.place(relx= 0.1, rely= 0.1, relwidth= 0.8, relheight= 0.2)
