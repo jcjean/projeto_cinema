8451b28e46f45bd97e37ef7417851745a26db0d1
Draraedus <adriel2002santos@gmail.com>
2022-05-20 18:17:11 -0300

    Começo da interface

--- src/Application.py
+++ src/Application.py
@@ -1,11 +1,12 @@
 from tkinter import *
-from PIL import Image, ImageTk
+from PIL import ImageTk, Image  
 
 root = Tk()
 
 class Application:
     def __init__(self):
         self.root = root
+        self.imgBg = ImageTk.PhotoImage(Image.open("src/assets/imgs/background.png"))
         self.tela()
         self.rooms()
         self.addRoom()
@@ -17,12 +18,10 @@ class Application:
         self.root.geometry("788x988")
         self.root.resizable(True, True)
         self.root.minsize(width=488, height=788)
-        bg = ImageTk.PhotoImage(Image.open("C:/Users/Pichau/Documents/Estudos/Faculdade/POO/Projeto Final/src/background.jpg").resize((488, 788)))
-        label1 = Label(root, image = bg) 
+        self.root.iconbitmap('src/assets/icons/icon.ico')
+        label1 = Label(root, image = self.imgBg) 
         label1.place(x=0, y=0, relwidth=1, relheight=1)
-    # def tools(self):
-    #     self.frameOfTools = Frame(self.root)
-    #     self.frameOfTools.place(relx= 0.1, rely= 0.1, relwidth= 0.8, relheight= 0.2)
+
     def rooms(self):
         self.frameRooms = Frame(self.root)
         self.frameRooms.place(relx= 0.1, rely= 0.1, relwidth= 0.8, relheight= 0.2)
@@ -33,11 +32,5 @@ class Application:
         self.btAddRoom = Button(self.frameAddRoom, text="Adicionar")
         self.btAddRoom.place (relx= 0.2, rely = 0.8, relwidth= 0.1, relheight=0.15)
 
-    # def seats(self):
-    #     self.frameSeats = Frame(self.root)
-    #     self.frameSeats.place(relx= 0.1, rely= 0.1, relwidth= 0.8, relheight= 0.2)
-    # def ownSeat(self):
-    #     self.frameOwnSeat = Frame(self.root)
-    #     self.frameOwnSeat.place(relx= 0.1, rely= 0.1, relwidth= 0.8, relheight= 0.2)
 
 Application()
